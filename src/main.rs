a484a49aee6596edbb518351811bbaae4cb327e4
Eugine Park <pkeugine@gmail.com>
2021-01-11 23:22:58 +0900

    feat(main): get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    print!("숫자를 입력해주세요 : ");
+    io::stdout().flush().unwrap();
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("숫자를 입력받지 못했습니다.");
+
+    println!("당신이 입력한 숫자입니다 : {}", guess);
 }
